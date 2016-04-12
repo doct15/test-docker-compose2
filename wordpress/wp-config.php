ba2a58712ed87a78c54afeab2d0b77618c2108d6
Brian McGehee <doc@tavian.com>
2016-04-12 15:49:33 -0700

    Second

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,23 +1,23 @@
 <?php
-define('DB_NAME', 'wordpress');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', "db:3306");
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
+define('DB_NAME', '{{WP_DB_NAME}}');
+define('DB_USER', '{{WP_DB_USER}}');
+define('DB_PASSWORD', '{{WP_DB_PASSWORD}}');
+define('DB_HOST', "{{WP_DB_HOST}}");
+define('DB_CHARSET', '{{WP_DB_CHARSET}}');
+define('DB_COLLATE', '{{WP_DB_COLLATE}}');
 
-define('AUTH_KEY',         'ABRACADABRA');
-define('SECURE_AUTH_KEY',  'ABRACADABRA');
-define('LOGGED_IN_KEY',    'ABRACADABRA');
-define('NONCE_KEY',        'ABRACADABRA');
-define('AUTH_SALT',        'ABRACADABRA');
-define('SECURE_AUTH_SALT', 'ABRACADABRA');
-define('LOGGED_IN_SALT',   'ABRACADABRA');
-define('NONCE_SALT',       'ABRACADABRA');
+define('AUTH_KEY',         '{{WP_AUTH_KEY}}');
+define('SECURE_AUTH_KEY',  '{{WP_SECURE_AUTH_KEY}}');
+define('LOGGED_IN_KEY',    '{{WP_LOGGED_IN_KEY}}');
+define('NONCE_KEY',        '{{WP_NONCE_KEY}}');
+define('AUTH_SALT',        '{{WP_AUTH_SALT}}');
+define('SECURE_AUTH_SALT', '{{WP_SECURE_AUTH_SALT}}');
+define('LOGGED_IN_SALT',   '{{WP_LOGGED_IN_SALT}}');
+define('NONCE_SALT',       '{{WP_NONCE_SALT}}');
 
 $table_prefix  = 'wp_';
-define('WPLANG', '');
-define('WP_DEBUG', false);
+define('WPLANG', '{{WP_WPLANG}}');
+define('WP_DEBUG', {{WP_WP_DEBUG}});
 
 if ( !defined('ABSPATH') )
   define('ABSPATH', dirname(__FILE__) . '/');
